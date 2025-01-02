743e9c039eefffabf1957ba41b8e2af0bd6eeb96
keshavkumar11 <121555063+keshavkumar11@users.noreply.github.com>
2025-01-02 12:00:09 +0530

    namePrinting.js

--- namePrinting.js
+++ namePrinting.js
@@ -1,6 +1,8 @@
 // KESHAV
 
 for(let i=0;i<5;i++){
+
+    // PRINTING "K"
     for(let k=0;k<5;k++){
         if ((i==2&&k<=2) || (i==3&&k==3) || (i==4&&k==4) || (i<=4&&k==0) || (i==0&&k==4) || (i==1&&k==3)) {
             process.stdout.write("*");
@@ -10,6 +12,7 @@ for(let i=0;i<5;i++){
     }
     process.stdout.write("  ");
 
+    // PRINTING "E"
     for(let e=0;e<5;e++){
         if ((i<=4&&e==0) || (i==0&&e<=4) || (i==2&&e<=4) || (i==4&&e<=4)) {
             process.stdout.write("*");
@@ -19,6 +22,7 @@ for(let i=0;i<5;i++){
     }
     process.stdout.write("  ");
 
+    // PRINTING "S"
     for(let s=0;s<5;s++){
         if ((i==0&&s<=4) || (i==1&&s==0) || (i==2&&s<=4) || (i==3&&s==4) || (i==4&&s<=4)) {
             process.stdout.write("*");
@@ -28,6 +32,7 @@ for(let i=0;i<5;i++){
     }
     process.stdout.write("  ");
 
+    // PRINTING "H"
     for(let h=0;h<5;h++){
         if ((i<=4&&h==0) || (i<=4&&h==4) || (i==2&&h<=4)) {
             process.stdout.write("*");
@@ -37,6 +42,7 @@ for(let i=0;i<5;i++){
     }
     process.stdout.write("  ");
 
+    // PRINTING "A"
     for(let a=0;a<5;a++){
         if ((i==0&&a==2) || (i==1&&(a==1||a==3)) || (i==2&&a<=4) || ((i>=2&&i<=4)&&(a==0||a==4))) {
             process.stdout.write("*");
@@ -46,6 +52,7 @@ for(let i=0;i<5;i++){
     }
     process.stdout.write("  ");
 
+    // PRINTING "V"
     for(let v=0;v<5;v++){
         if ((i<=2&&(v==0||v==4))  || (i==3&&(v==1||v==3)) || (i==4&&v==2)) {
             process.stdout.write("*");
